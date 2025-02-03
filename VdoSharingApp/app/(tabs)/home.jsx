3e5d7c8e05273680c0295b3edef2f316f813611d
Pulkit-s21 <pulkitsaxena795@gmail.com>
2025-02-03 22:46:32 +0530

    New things about Flatlist, creating home page

--- VdoSharingApp/app/(tabs)/home.jsx
+++ VdoSharingApp/app/(tabs)/home.jsx
@@ -1,49 +1,86 @@
 import { useGlobalContext } from "@/context/GlobalProvider"
 import { StatusBar } from "expo-status-bar"
-import { View, Text, FlatList, Image } from "react-native"
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  RefreshControl,
+} from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 import { images } from "@/constants"
 import { SearchInput } from "@/components/SearchInput"
+import { Trending } from "@/components/Trending"
+import { EmptyComponent } from "@/components/EmptyComponent"
+import { useState } from "react"
+import { getAllPosts } from "@/lib/appwrite"
+import { useAppwrite } from "@/lib/useAppwrite"
+import { VideoCard } from "@/components/VideoCard"
 
 const Home = () => {
+  const { data: posts, refetch } = useAppwrite(getAllPosts)
+
   const { user } = useGlobalContext()
 
-  return (
-    <SafeAreaView className="bg-primary">
-      <FlatList
-        data={[{ id: 1 }, { id: 2 }]}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Text className="text-3xl">{item.id}</Text>}
-        ListHeaderComponent={() => (
-          <View className="my-6 px-4 space-y-6">
-            <View className="justify-between items-start flex-row mb-6 px-2">
-              <View className="gap-2">
-                <Text className="text-sm text-gray-100 font-pmedium">
-                  Welcome Back
-                </Text>
-                <Text className="text-3xl text-white font-psemibold">
-                  {user.username}
-                </Text>
-              </View>
+  const [refreshing, setRefreshing] = useState(false)
 
-              <View>
-                <Image
-                  source={images.logoSmall}
-                  className="w-9 h-10"
-                  resizeMode="contain"
-                />
-              </View>
-            </View>
+  const onRefresh = async () => {
+    setRefreshing(true)
+    await refetch()
+    setRefreshing(false)
+  }
 
-            <SearchInput placeHolder={"Search for a video topic"} />
-
-            <View className="w-full flex-1 pt-5">
-              <Text className="text-lg text-gray-100 font-pregular mt-5">
-                Trending Videos
-              </Text>
-            </View>
+  const headerComponent = () => {
+    return (
+      <View className="my-6 px-4 space-y-6">
+        {/* msg and icon div */}
+        <View className="justify-between items-start flex-row mb-6 px-2">
+          <View className="gap-2">
+            <Text className="text-sm text-gray-100 font-pmedium">
+              Welcome Back
+            </Text>
+            <Text className="text-3xl text-white font-psemibold">
+              {user.username}
+            </Text>
           </View>
-        )}
+
+          <View>
+            <Image
+              source={images.logoSmall}
+              className="w-9 h-10"
+              resizeMode="contain"
+            />
+          </View>
+        </View>
+
+        <SearchInput placeHolder={"Search for a video topic"} />
+
+        <View className="w-full flex-1 pt-5">
+          <Text className="text-lg text-gray-100 font-pregular mt-5">
+            Trending Videos
+          </Text>
+        </View>
+        <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }]} />
+      </View>
+    )
+  }
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={posts} // data for renderItem
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard video={item} />}
+        ListHeaderComponent={headerComponent}
+        ListEmptyComponent={
+          <EmptyComponent
+            title={"No videos found"}
+            subTitle={"Be the first one to upload a video"}
+          />
+        }
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       />
       <StatusBar style="light" />
     </SafeAreaView>
